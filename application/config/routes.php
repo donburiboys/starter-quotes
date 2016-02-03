8331208dbf1f24788c7042ab6e50a7d537b4e977
Derrick <drick89@gmail.com>
2016-02-02 19:12:43 -0800

    #6 Dunno completed

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,6 +41,15 @@
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 $route['sleep'] = "first/zzz";
+$route['dunno'] = function() {
+    $source = './data/dunno.jpg'; // an image you provide
+    // set the mime type for that image
+    header("Content-type: image/jpeg"); 
+    header('Content-Disposition: inline');
+    readfile($source); // dish it
+    die(); // and we don't have to go any further
+};
+
 
 
 /* End of file routes.php */
